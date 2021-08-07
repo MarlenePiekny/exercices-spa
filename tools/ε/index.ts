0c68f41b8f13fb93f02c38b6afd2cd5f56e6d729
rdhox <admin@rdhox.io>
2021-08-07 20:14:19 +0200

    functional

--- tools/ε/index.ts
+++ tools/ε/index.ts
@@ -4,12 +4,11 @@ import {
   Children
 } from './frameworkTypes';
 import elementToString from './elementToString';
-import { compare } from './compare';
 
 // Variables
 let nbComponents = 0;
-let rootH;
 let actualDom: Element | string | null = null;
+let actualDomHTML = null;
 let haveToUpdate: number | null = null;
 const INST_COMP = {};
 
@@ -87,6 +86,9 @@ function createHTMLTag(e: Element):HTMLElement | string {
     if(!RESERVED_TAG_WORDS.includes(e.container)) {
       // html tag
       const tag = document.createElement(e.container);
+      if (e._id) {
+        tag.setAttribute('data-e', e._id.toString());
+      }
       Object.keys(e.attributes).forEach(name => {
         if (name === 'class' && typeof e.attributes[name] === 'string') {
           tag.setAttribute(name, e.attributes[name] as string);
@@ -122,19 +124,22 @@ function createHTMLTag(e: Element):HTMLElement | string {
 
   // Case of Element being a Component
   if (typeof e.container === 'function') {
-    const instanceComp = new e.container({...e.attributes, children: e.children});
-    INST_COMP[e._id] = instanceComp;
-    const renderElement = instanceComp.render();
+    const renderElement = INST_COMP[e._id].render();
     return createHTMLTag(renderElement);
   }
 }
 
 function getRenderElementFromComponentElement(e: Element, register:boolean): Element {
-  const instanceComp = new e.container({...e.attributes, children: e.children});
-  if(register) {
-    INST_COMP[instanceComp._id] = instanceComp;
+  let render;
+  if (INST_COMP[e._id]) {
+    render = INST_COMP[e._id].render();
+  } else {
+    const instanceComp = new e.container({...e.attributes, children: e.children});
+    if(register) {
+      INST_COMP[instanceComp._id] = instanceComp;
+    }
+    render = instanceComp.render();
   }
-  const render = instanceComp.render();
   return render;
 }
 
@@ -160,9 +165,9 @@ function activateElement(e: Element, register: boolean = true) {
 function start(rootComponent: any, rootHtml: HTMLElement): void {
   const instance = new rootComponent();
   const element = instance.render();
-  rootH = rootHtml;
   const elementOn = activateElement(element);
   const site = elementToHTML(elementOn);
+  actualDomHTML = site;
   if (!actualDom) {
     if (typeof site === 'string') {
       rootHtml.innerHTML = site;
@@ -179,39 +184,24 @@ function startUpdateLoop() {
     updateDom();
     clearTimeout(unsub);
     startUpdateLoop();
-  }, 1000);
+  }, 60);
 }
 
-function parseDomAndReplace(element, newElement) {
-  const copy = JSON.parse(JSON.stringify(element));
-  if(typeof copy.children === 'object') {
-    for (let i = 0; i < copy.children.length; i++) {
-      const child = copy.children[i];
-      if(child._id && child._id === haveToUpdate) {
-        copy.children[i] = newElement;
-        return copy;
-      } else {
-        return parseDomAndReplace(child, newElement);
-      }
-    }
-  }
-}
 
 function findCompAndReturnNewDom(element: Element) {
   const elementToReplace = INST_COMP[haveToUpdate].render();
-  const active = activateElement(elementToReplace, false);
-  return parseDomAndReplace(element, active);
+  return activateElement(elementToReplace, false);
 }
+
 
 function updateDom() {
   if (haveToUpdate && typeof actualDom === 'object') {
-    const nextDom = findCompAndReturnNewDom(actualDom);
-    if(!compare(actualDom, nextDom)) {
-      const site = elementToHTML(nextDom);
-      rootH.innerHTML = '';
-      rootH.appendChild(site);
-      actualDom = nextDom;
-    }
+    const newPortion = findCompAndReturnNewDom(actualDom);
+    const newFragment = elementToHTML(newPortion) as HTMLElement;
+    const id = newFragment.getAttribute('data-e')
+    const bloc = document.querySelector(`[data-e="${id}"]`);
+    bloc.innerHTML = '';
+    bloc.append(...newFragment.childNodes);
     haveToUpdate = null;
   }
 }
@@ -219,7 +209,6 @@ function updateDom() {
 export {
   Component,
   Element,
-  compare,
 };
 
 export default {
