4a39a4d3554ff3a13859c82e96f0429a44cc8650
MarlenePiekny <marlene.piekny@gmail.com>
2021-08-12 11:25:48 +0200

    feat: display HTML from virtual DOM

--- components/Title.ts
+++ components/Title.ts
@@ -3,7 +3,7 @@ import * as ε from '../tools/ε/';
 class Title extends ε.Component {
 
   render() {
-    return ε.createElement('h1', {}, ['hello']);
+    return ε.createElement('h1', {}, ['Hello World']);
   }
 }
 
--- tools/ε/index.ts
+++ tools/ε/index.ts
@@ -1,58 +1,85 @@
-import { Element } from './frameworkTypes';
+import {Element} from './frameworkTypes';
 
 // parent class for components
 abstract class Component {
-  abstract render(): Element
+    abstract render(): Element
 }
 
 function createElement(name, attributes, children) {
-  return {
-    name,
-    attributes,
-    children
-  };
+    return {
+        name,
+        attributes,
+        children
+    };
 };
 
-function elementsToHTML(virtualDom) {
-  // TODO
+function elementsToHTML(VDOM) {
+    if (!VDOM.children) {
+        let e = document.createElement(VDOM.name);
+        for ( const attribute in VDOM.attributes) {
+            console.log(`${attribute} : ${VDOM.attributes[attribute]}`);
+            e.setAttribute(`${attribute}` , `${VDOM.attributes[attribute]}`)
+        }
+        return e;
+    } else {
+        console.log('coucou');
+        let e = document.createElement(VDOM.name);
+        for ( const attribute in VDOM.attributes) {
+            console.log(`${attribute} : ${VDOM.attributes[attribute]}`);
+            e.setAttribute(`${attribute}` , `${VDOM.attributes[attribute]}`)
+        }
+        VDOM.children.forEach(child=> {
+            if (typeof child === 'string') {
+                e.innerHTML = child;
+            } else {
+                e.append(elementsToHTML(child))
+            }
+        })
+        return e;
+    }
 }
+
 
 function getElement(element) {
-  let elementToReturn;
-  if (typeof element === 'string') {
-    elementToReturn = element;
-  } else if(typeof element.name === 'string') {
-    const childrenCol = [];
-    element.children.forEach(e => {
-      childrenCol.push(getElement(e));
-    });
-    element.children = childrenCol;
-    elementToReturn = element;
-  } else {
-    const instance = new element.name();
-    const render = instance.render();
-    elementToReturn = getElement(render);
-  }
+    let elementToReturn;
+    if (typeof element === 'string') {
+        elementToReturn = element;
+    } else if (typeof element.name === 'string') {
+        const childrenCol = [];
+        element.children.forEach(e => {
+            childrenCol.push(getElement(e));
+        });
+        element.children = childrenCol;
+        elementToReturn = element;
+    } else {
+        const instance = new element.name();
+        const render = instance.render();
+        elementToReturn = getElement(render);
+    }
 
-  return elementToReturn;
+    return elementToReturn;
 }
-
 
 
 function getVirtualDom(element) {
-  return getElement(element);
+    return getElement(element);
+}
+
+function letsGo(root, site) {
+    root.append(site);
 }
 
 function start(rootComponent, rootHtml: HTMLElement): void {
-  const rootInstance = new rootComponent();
-  const rootRender = rootInstance.render();
-  const virtualDom = getVirtualDom(rootRender);
-  const site = elementsToHTML(virtualDom);
-  console.log(site);
+    const rootInstance = new rootComponent();
+    const rootRender = rootInstance.render();
+    const virtualDom = getVirtualDom(rootRender);
+    const site = elementsToHTML(virtualDom);
+    letsGo(rootHtml, site);
 }
 
+
 export {
-  Component,
-  start,
-  createElement
+    Component,
+    start,
+    createElement
 };
